d059a095a13b300645b7cd9e330f7fe5438bda3e
Gennadiy Dubina <gdubina@dataart.com>
2017-02-09 14:26:16 +0000

    fix multy containers

--- installer/install_osm.sh
+++ installer/install_osm.sh
@@ -14,5 +14,6 @@ sudo touch /etc/default/lxd-bridge
 export COMMIT_ID=$1
 export HUB_REPO=datelco
 export NET_MTU=${2:-1500}
-
+export API_SERVER=`ip addr show ens3 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/.[0-9]\+' | grep -o [0-9].* | cut -d '/' -f1`
+ 
 docker-compose -p osm -f ../docker-compose.yml up -d
